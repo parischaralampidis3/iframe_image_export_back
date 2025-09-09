3d71999c260e4c9af8e800300d637951709911b7
Paris Charalampidis <parou.char@gmail.com>
2025-09-09 13:42:37 +0300

    preparing deployment

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const express = require("express");
 const routes = require("./routes/captureRoutes");
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 app.use(cors(
   {
